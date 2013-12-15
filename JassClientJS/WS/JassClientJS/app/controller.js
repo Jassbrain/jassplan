1dea70320c7ff827dc7bc7946972f2427363116b
Pablo Elustondo <pablo@jassbrain.com>
2013-12-15 16:39:12 -0500

    added lastest code to JS client

---
+++ JassClientJS/WS/JassClientJS/app/controller.js
@@ -0,0 +1,62 @@
+/**
+ * Created by pablo on 12/15/13.
+ */
+
+var Jassplan = Jassplan || {};
+
+Jassplan.controller = (function () {
+
+    var notesListPageId = "notes-list-page";
+    var notesListSelector = "#notes-list-content";
+
+    var renderNotesList = function () {
+        var dummyNotesCount = 10,
+            note,
+            i;
+        var view = $(notesListSelector);
+        view.empty();
+        var ul = $("<ul id=\"notes-list\" data-role=\"listview\"></ul>").appendTo(view);
+        for (i = 0; i < dummyNotesCount; i += 1) {
+            $("<li>"
+            + "<a href=\"index.html#note-editor-page?noteId=" + i + "\">"
+            + "<div>Note title " + i + "</div>"
+            + "<div class=\"list-item-narrative\">Note Narrative " + i + "</div>"
+            + "</a>"
+            + "</li>").appendTo(ul);
+        }
+
+
+
+        ul.listview();
+
+
+
+    };
+
+    var onPageChange = function (event, data) {
+        var toPageId = data.toPage.attr("id");
+        switch (toPageId) {
+            case notesListPageId:
+                renderNotesList();
+                break;
+        }
+    };
+
+    var init = function () {
+        $(document).bind("pagechange", onPageChange);
+    };
+
+    var public = {
+        init: init
+    };
+
+    return public;
+
+})();
+
+$(document).bind("mobileinit", function () {
+    Jassplan.controller.init();
+
+});
+
+
